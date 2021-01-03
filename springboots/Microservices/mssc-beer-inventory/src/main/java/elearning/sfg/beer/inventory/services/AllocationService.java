c5830e909b48ce5c68f132bd46bf59c50e865add
mkejeiri <kejeiri@gmail.com>
2021-01-03 18:44:11 +0100

    Added Cancel Order Consumer, i.e. Deallocate the Inventory in the invertory µservice

--- springboots/Microservices/mssc-beer-inventory/src/main/java/elearning/sfg/beer/inventory/services/AllocationService.java
+++ springboots/Microservices/mssc-beer-inventory/src/main/java/elearning/sfg/beer/inventory/services/AllocationService.java
@@ -4,4 +4,7 @@ import elearning.sfg.beer.brewery.dtos.BeerOrderDto;
 
 public interface AllocationService {
     Boolean allocateOrder(BeerOrderDto beerOrderDto);
+
+    void deallocateOrder(BeerOrderDto beerOrderDto);
+
 }
---
+++ springboots/Microservices/mssc-beer-inventory/src/main/java/elearning/sfg/beer/inventory/services/DeallocationListener.java
@@ -0,0 +1,21 @@
+package elearning.sfg.beer.inventory.services;
+
+import elearning.sfg.beer.brewery.events.DeallocateOrderRequested;
+import elearning.sfg.beer.inventory.config.JmsConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class DeallocationListener {
+
+    private final AllocationService allocationService;
+
+    @JmsListener(destination = JmsConfig.DEALLOCATE_ORDER_QUEUE)
+    public void listen(DeallocateOrderRequested event){
+        allocationService.deallocateOrder(event.getBeerOrderDto());
+    }
+}
